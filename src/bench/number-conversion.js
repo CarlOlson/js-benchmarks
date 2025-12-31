db867e31e057de679dc5e8ef871bf315f3c6fd6d
CarlOlson <CarlOlson@users.noreply.github.com>
2025-12-30 23:46:46 -0600

    add number-conversion benchmark

---
+++ src/bench/number-conversion.js
@@ -0,0 +1,26 @@
+import { run, bench, lineplot } from "mitata";
+import { shuffle } from "../util.js";
+import isInteractive from "is-interactive";
+
+const minSize = 1024 * 4;
+const maxSize = 1024 * 64;
+
+lineplot(() => {
+  bench("unary plus ($size)", function* (state) {
+    const size = state.get("size");
+    const input = Array.from({ length: size }).map(() => String(Math.random()));
+    shuffle(input);
+
+    yield () => +input;
+  }).range("size", minSize, maxSize);
+
+  bench("number constructor ($size)", function* (state) {
+    const size = state.get("size");
+    const input = Array.from({ length: size }).map(() => String(Math.random()));
+    shuffle(input);
+
+    yield () => Number(input);
+  }).range("size", minSize, maxSize);
+});
+
+await run({ format: isInteractive() ? "mitata" : "json" });
